0ae7b71d8b268dabf9c5cbf66c64e512cceaf567
Tom Watkins <twwatkins@btinternet.com>
2025-02-23 23:07:29 +0000

    Dark/light mode switcher

---
+++ src/components/ThemeButton.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    let darkMode = false;
+    let body: HTMLBodyElement;
+
+    onMount(() => {
+        darkMode = document.documentElement.classList.contains("dark");
+        body = document.querySelector("body") as HTMLBodyElement;
+    });
+
+    function handleClick() {
+        body.style.transition = "color .1s, background-color .3s";
+        if (darkMode) {
+            document.documentElement.classList.remove("dark");
+            localStorage.theme = "light";
+            darkMode = false;
+        } else {
+            document.documentElement.classList.add("dark");
+            localStorage.theme = "dark";
+            darkMode = true;
+        }
+    }
+</script>
+
+<button
+    class="dark-mode-button"
+    aria-label={`Switch to ${darkMode ? "light" : "dark"} mode`}
+    on:click={handleClick}
+>
+</button>
+
+<style>
+    .dark-mode-button {
+        height: 1.5rem;
+        aspect-ratio: 1;
+        background-image: url("../assets/moon.svg");
+        cursor: pointer;
+    }
+
+    :global(:root.dark) .dark-mode-button {
+        background-image: url("../assets/sun.svg");
+    }
+</style>
